2f72251ebd01f52b66a91f417359bdbce11da7c3
arraayyy <20102056@usc.edu.ph>
2022-02-27 12:22:30 +0800

    final

--- test.py
+++ test.py
@@ -13,26 +13,25 @@ class currencyConverter: # Create class
         
         # Create objects and add entry functions
         self.amountToConvert = StringVar()
-        Entry(window, textvariable = self.amountToConvert, justify = RIGHT).grid(row = 1,column = 2)
+        Entry(window, textvariable = self.amountToConvert, justify = RIGHT).grid(row=1,column=2)
         self.conversionRate = StringVar()
-        Entry(window, textvariable = self.conversionRate, justify = RIGHT).grid(row = 2,column = 2)
+        Entry(window, textvariable = self.conversionRate, justify = RIGHT).grid(row=2,column=2)
         self.convertedAmount = StringVar()
-        lblConvertedAmount = Label(window, font = "Helvetica 12 bold", bg = "yellow", textvariable = self.convertedAmount).grid(row = 3,column = 2,sticky = E)
-        
-        # Create convert and clear buttons . When clicked they will call their respective functions.
-        btnConvertedAmount = Button(window, text = "Convert",font = "Helvetica 12 bold",bg = "blue",fg = "white",command = self.ConvertedAmount).grid(row = 4,column = 2,sticky = E)
-        btndelete_all = Button(window, text = "Clear", font = "Helvetica 12 bold", bg = "red",fg = "white",command = self.delete_all).grid(row = 4,column = 6,padx = 25,pady = 25,sticky = E)
+        lblConvertedAmount = Label(window,  bg = "yellow", font = "Helvetica 12 bold", textvariable = self.convertedAmount).gird(row = 3, column = 2, sticky = E)
 
-        window.mainloop()  # Runs the application program
+        # Create convert and clear buttons. When clicked, the convert and clear functions are called
+        btcConvertedAmount = Button(window, text = "Convert", font = "Helvetica 12 bold", bg = "blue", fg = "white", command = self.ConvertedAmount).grid(row = 4, column = 1, sticky = E)
+        btdelete_all = Button(window, text = "Clear", font = "Helvetica 12 bold", bg = "red", fg = "white", command = self.delete_all).grid(row = 4, column = 6, padx = 25, pady = 25, sticky = E)
 
-        # Function to do the conversion. Stores inputs and performs conversion
+        window.mainloop()
+
     def ConvertedAmount(self):
-        amount = float(self.conversionRate.get())
-        convertedAmount =float(self.amounttoConvert.get()) * amount
+        amt = float(self.conversionRate.get())
+        convertedAmount =float(self.amounttoConvert.get()) * amt
         self.convertedAmount.set(format(convertedAmount, '10.2f'))
 
         # Function to clear inputs
-    def delete_all(self):
+    def cdelete_all(self):
         self.amounttoConvert.set("")
         self.conversionRate.set("")
         self.convertedAmount.set("")
