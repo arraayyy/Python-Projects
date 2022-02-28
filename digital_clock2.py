bd47eb1c830c8a170bca882e294909af2f65ea67
arraayyy <20102056@usc.edu.ph>
2022-02-28 19:31:06 +0800

    initial commit

--- digital_clock2.py
+++ digital_clock2.py
@@ -1,4 +1,5 @@
 from cmath import exp
+from time import strftime
 from tkinter import *
 from datetime import datetime
 
@@ -10,6 +11,14 @@ w.title("Digital Clock")
 f1 = Frame(w, width = 750, height = 200, bg = "#0e10113")
 f1.place(x = 0, y = 0)
 f1.pack(expand = True)
+
+def time():
+    a = strftime("%H   :   %M   :   %S")
+    lbl.config(text = a)
+    lbl.after(1000, time)
+
+lbl = Label(f1, font = ('Century Gothic', 60), bg = "black", fg = "#d3d3d3")
+lbl.place(x = 270, y = 35)
 
 
 w.mainloop()
