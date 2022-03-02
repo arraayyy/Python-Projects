123df87d337d3438bed01eb68e5c99da32de4190
arraayyy <20102056@usc.edu.ph>
2022-03-02 18:38:07 +0800

    modules

---
+++ slideshow.py
@@ -0,0 +1,3 @@
+from itertools import cycle # itertools - module contains functions for creating iterators for efficient looping
+                            # cycle - creates an iterator that returns elements from the iterable
+import tkinter as tk
