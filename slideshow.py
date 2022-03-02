c48007551b22aebe5b3f0fcc413394a217d3c5c9
arraayyy <20102056@usc.edu.ph>
2022-03-02 19:25:45 +0800

    final commit

--- slideshow.py
+++ slideshow.py
@@ -5,9 +5,32 @@ import tkinter as tk
 class App(tk.Tk):
     def __init__(self, image_files, x, y, delay):
         tk.Tk.__init__(self)
-        self.geometry('+ {}+ {}'format(x, y))
+        self.geometry('+{}+{}'.format(x, y))
         self.delay = delay
         self.pictures = cycle((tk.PhotoImage(file=image), image) # PhotoImage - class for Tkinter images
         for image in image_files)
         self.pictures_display = tk.Label(self)
         self.pictures_display.pack()
+
+    def show_slides(self):
+        img_object, img_name = next(self.pictures)
+        self.pictures_display.config(image=img_object)
+        self.title(img_name)
+        self.after(self.delay, self.show_slides) # after - schedules a function to be called after the given time
+
+    def run(self):
+        self.mainloop()
+
+    delay = 3500
+
+    image_files = [
+        '1.jpg',
+        '2.gif',
+        '3.gif',
+    ]
+
+x = 100
+y = 50
+app = App(image_files, x, y, delay)
+app.show_slides()
+app.run()
