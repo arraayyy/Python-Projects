7d4400e099a740d010ff7373998bad3f095451de
arraayyy <20102056@usc.edu.ph>
2022-03-02 18:56:10 +0800

    class and functions

--- slideshow.py
+++ slideshow.py
@@ -1,3 +1,13 @@
 from itertools import cycle # itertools - module contains functions for creating iterators for efficient looping
                             # cycle - creates an iterator that returns elements from the iterable
 import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self, image_files, x, y, delay):
+        tk.Tk.__init__(self)
+        self.geometry('+ {}+ {}'format(x, y))
+        self.delay = delay
+        self.pictures = cycle((tk.PhotoImage(file=image), image) # PhotoImage - class for Tkinter images
+        for image in image_files)
+        self.pictures_display = tk.Label(self)
+        self.pictures_display.pack()
