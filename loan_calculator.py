439b4a4578d297a45b75b7325030bce4cbd9986a
arraayyy <20102056@usc.edu.ph>
2022-03-03 20:33:39 +0800

    functions

--- loan_calculator.py
+++ loan_calculator.py
@@ -25,4 +25,28 @@ class LoanCalculator:
 
         btComputePayment = Button(window, text="Compute Payment", bg="red", fg="white", font='Helvetica 14 bold', command= self.computePayment).grid(row=6, column=2, sticky=E)
         btClear = Button(window, text="Clear", bg="blue", fg="white", font='Helvetica 14 bold', command= self.delete_all).grid(row=6, column=8, sticky=E, padx=20, pady=20)
-        
+
+        window.mainloop()
+
+    def computePayment(self):
+        monthlyPayment = self.getMonthlyPayment(
+            float(self.loanAmountVar.get()),
+            float(self.annualInterestRateVar.get())/1200,
+            int(self.numberOfYearsVar.get()))
+
+        self.monthlyPaymentVar.set(format(monthlyPayment, '10.2f'))
+        totalPayment = float(self.monthlyPaymentVar.get()) * 12 * int(self.numberOfYearsVar.get())
+        self.totalPaymentVar.set(format(totalPayment, '10.2f'))
+
+    def getMonthlyPayement(self, loanAmount, monthlyInterestRate, numberofYears):
+        monthlyPayment = loanAmount * monthlyInterestRate / (1-1/(1 + monthlyInterestRate)** (numberofYears * 12))
+        return monthlyPayment;
+    
+    def delete_all(self):
+        self.annualInterestRateVar.set("")
+        self.numberOfYearsVar.set("")
+        self.loanAmountVar.set("")
+        self.monthlyPaymentVar.set("")
+        self.totalPaymentVar.set("")
+
+LoanCalculator()
