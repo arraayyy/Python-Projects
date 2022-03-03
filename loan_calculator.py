b39efd12e89466adf2ba7a111a7cc44bcbaf9d47
arraayyy <20102056@usc.edu.ph>
2022-03-03 20:23:37 +0800

    buttons

--- loan_calculator.py
+++ loan_calculator.py
@@ -22,3 +22,7 @@ class LoanCalculator:
         lblMonthlyPayment = Label(window, font='Helvetica 12 bold', bg="light blue", textvariable=self.monthlyPaymentVar).grid(row=4, column=2, sticky=E)
         self.totalPaymentVar = StringVar()
         lblTotalPayment = Label(window, font='Helvetica 12 bold', bg="light blue", textvariable=self.totalPaymentVar).grid(row=5, column=2, sticky=E)
+
+        btComputePayment = Button(window, text="Compute Payment", bg="red", fg="white", font='Helvetica 14 bold', command= self.computePayment).grid(row=6, column=2, sticky=E)
+        btClear = Button(window, text="Clear", bg="blue", fg="white", font='Helvetica 14 bold', command= self.delete_all).grid(row=6, column=8, sticky=E, padx=20, pady=20)
+        
