9e34412607536b86102fe5bf4e9288eaa28a5f14
arraayyy <20102056@usc.edu.ph>
2022-03-03 20:05:12 +0800

    initial commit

---
+++ loan_calculator.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+class LoanCalculator:
+    def __init__(self):
+        window = Tk()
+        window.title("Loan Calculator")
+        window.configure(background="light blue")
